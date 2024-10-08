56cc03a34fae75f3f58116309062a3857c94f150
Matt Glenn <highopenended@gmail.com>
2024-10-08 18:25:08 -0400

    Implemented DELETE api call

--- frontend/components/DogsList.js
+++ frontend/components/DogsList.js
@@ -4,28 +4,38 @@ export default function DogsList() {
   const [dogs, setDogs] = useState([]);
 
   useEffect(() => {
-    function getDogs() {
-      fetch("http://localhost:3003/api/dogs")
-        .then((res) => {
-          if (!res.ok) {
-            throw new Error(`Ouch, status ${res.status}`);
-          }
-          const contentType = res.headers.get("Content-Type");
-          if (contentType.includes("application/json")) {
-            console.log("We are getting dogs... but they have not arrived yet!");
-            return res.json(); // res.json returns a promise
-          }
-        })
-        .then((data) => {
-          console.log(data);
-          setDogs(data);
-        })
-        .catch((err) => {
-          console.log("Something went wrong GETing dogs...", err);
-        });
-    }
     getDogs();
   }, []);
+
+  async function getDogs() {
+    fetch("http://localhost:3003/api/dogs")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`Ouch, status ${res.status}`);
+        }
+        const contentType = res.headers.get("Content-Type");
+        if (contentType.includes("application/json")) {
+          return res.json(); // res.json returns a promise
+        }
+      })
+      .then((data) => setDogs(data))
+      .catch((err) => {
+        console.log("Something went wrong GETing dogs...", err);
+      });
+  }
+
+  const clickHandler_edit = (id) => {
+    console.log("Clicked Edit for id: ", id);
+  };
+
+  const clickHandler_delete = (id) => {
+    console.log("Clicked Delete for id: ", id);
+    fetch(`http://localhost:3003/api/dogs/${id}`, { method: "DELETE" })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error(`Couldn't delete id: ${id}`, err));
+    getDogs();
+  };
 
   return (
     <div>
@@ -33,11 +43,11 @@ export default function DogsList() {
       <ul>
         {dogs.map((dog) => {
           return (
-            <li key={dog.id}>
+            <li key={dog.id} id={dog.id}>
               {dog.name}, {dog.breed}, {dog.adopted ? "adopted" : "NOT adopted"}
               <div>
-                <button>Edit</button>
-                <button>Delete</button>
+                <button onClick={() => clickHandler_edit(dog.id)}>Edit</button>
+                <button onClick={() => clickHandler_delete(dog.id)}>Delete</button>
               </div>
             </li>
           );
