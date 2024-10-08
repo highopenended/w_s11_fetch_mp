4ea763975e4cc9c8229e28cf6093599ed64bf5fb
Matt Glenn <highopenended@gmail.com>
2024-10-08 18:03:28 -0400

    Implemented the dogBreeds option list API call

--- frontend/components/DogsList.js
+++ frontend/components/DogsList.js
@@ -1,49 +1,48 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 export default function DogsList() {
+  const [dogs, setDogs] = useState([]);
 
-  const [dogs, setDogs]=useState([])
-
-  useEffect(()=>{
+  useEffect(() => {
     function getDogs() {
-      fetch('http://localhost:3003/api/dogs')
-        .then(res => {
+      fetch("http://localhost:3003/api/dogs")
+        .then((res) => {
           if (!res.ok) {
-            throw new Error(`Ouch, status ${res.status}`)
+            throw new Error(`Ouch, status ${res.status}`);
           }
-          const contentType = res.headers.get('Content-Type')
-          if (contentType.includes('application/json')) {
-            console.log('We are getting dogs... but they have not arrived yet!')
-            return res.json() // res.json returns a promise
+          const contentType = res.headers.get("Content-Type");
+          if (contentType.includes("application/json")) {
+            console.log("We are getting dogs... but they have not arrived yet!");
+            return res.json(); // res.json returns a promise
           }
         })
-        .then(data=>{
-          console.log(data)
-          setDogs(data)
+        .then((data) => {
+          console.log(data);
+          setDogs(data);
         })
-        .catch(err => {
-          console.log('Something went wrong GETing dogs...', err)
-        })
+        .catch((err) => {
+          console.log("Something went wrong GETing dogs...", err);
+        });
     }
-    getDogs()
-  },[]) 
- 
+    getDogs();
+  }, []);
 
   return (
     <div>
       <h2>Dogs Shelter</h2>
       <ul>
-        {dogs.map(dog=>{
-          return <li>
+        {dogs.map((dog) => {
+          return (
+            <li key={dog.id}>
               {dog.name}, {dog.breed}, {dog.adopted ? "adopted" : "NOT adopted"}
               <div>
                 <button>Edit</button>
                 <button>Delete</button>
               </div>
             </li>
+          );
         })}
       </ul>
     </div>
-  )
+  );
 }
